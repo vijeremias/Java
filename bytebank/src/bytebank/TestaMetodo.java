009862e1c1a8e24dc885c3a66f4f0d24faf1c279
vijeremias <45375804+vijeremias@users.noreply.github.com>
2020-03-06 14:59:02 -0300

    add

--- bytebank/src/bytebank/TestaMetodo.java
+++ bytebank/src/bytebank/TestaMetodo.java
@@ -7,6 +7,10 @@ public class TestaMetodo {
 		conta.saldo = 100;
 		conta.deposita(100);
 		System.out.println(conta.saldo);
+		// conta.saca(1);
+		boolean conseguiuRetirar = conta.saca(23);
+		System.out.println(conta.saldo);
+		System.out.println(conseguiuRetirar);
 
 	}
 
