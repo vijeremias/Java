efdbaf44e2505dcf0f08122d97243d68e1438f77
vijeremias <vjeremiasbraz@gmail.com>
2020-03-06 15:49:08 -0300

    Merge branch 'AlteracaoDeContas' of https://github.com/vijeremias/Java into feature/ADIÇÃO_MAIS_ARQUIVOS
    # Conflicts:
    #	bytebank/src/bytebank/Conta.java
    #	bytebank/src/bytebank/TestaMetodo.java

--- bytebank/src/bytebank/Conta.java
+++ bytebank/src/bytebank/Conta.java
@@ -18,5 +18,19 @@ public class Conta {
 		}else {
 			return false;
 		}
+<<<<<<< HEAD
+=======
+	}
+	
+	public boolean transfere(double valor, Conta destino) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		}else {
+			return false;
+			
+		}
+>>>>>>> d186c383a778da86a7de39e314d2818abfff603b
 	}
 }
--- bytebank/src/bytebank/TestaMetodo.java
+++ bytebank/src/bytebank/TestaMetodo.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package bytebank;
 
 public class TestaMetodo {
@@ -15,3 +16,38 @@ public class TestaMetodo {
 	}
 
 }
+=======
+package bytebank;
+
+public class TestaMetodo {
+
+	public static void main(String[] args) {
+		Conta conta  = new Conta();
+		conta.saldo = 2000;
+		conta.deposita(200);
+		System.out.println(conta.saldo);
+		// conta.saca(1);
+		boolean conseguiuRetirar = conta.saca(100);
+		System.out.println(conta.saldo);
+		System.out.println(conseguiuRetirar);
+		
+		Conta outraConta = new Conta();
+		outraConta.saldo = 1000;
+		System.out.println(outraConta.saldo);
+		
+		
+		boolean sucessoTransferencia = outraConta.transfere(100, conta);
+		if(sucessoTransferencia) {
+			System.out.println("Transferencia com sucesso");
+		}else {
+			System.out.println("Faltou dinheiro pobre");
+		}
+		
+		System.out.println(outraConta.saldo);
+		System.out.println(conta.saldo);
+		
+
+	}
+
+}
+>>>>>>> d186c383a778da86a7de39e314d2818abfff603b
