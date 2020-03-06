30920f4dd20b3c0776f7a50202f2603c0b477cb7
vijeremias <vjeremiasbraz@gmail.com>
2020-03-06 15:25:04 -0300

    add

--- bytebank/src/bytebank/TestaMetodo.java
+++ bytebank/src/bytebank/TestaMetodo.java
@@ -4,13 +4,29 @@ public class TestaMetodo {
 
 	public static void main(String[] args) {
 		Conta conta  = new Conta();
-		conta.saldo = 100;
+		conta.saldo = 2000;
 		conta.deposita(100);
 		System.out.println(conta.saldo);
 		// conta.saca(1);
-		boolean conseguiuRetirar = conta.saca(23);
+		boolean conseguiuRetirar = conta.saca(100);
 		System.out.println(conta.saldo);
 		System.out.println(conseguiuRetirar);
+		
+		Conta outraConta = new Conta();
+		outraConta.saldo = 1000;
+		System.out.println(outraConta.saldo);
+		
+		
+		boolean sucessoTransferencia = outraConta.transfere(100, conta);
+		if(sucessoTransferencia) {
+			System.out.println("Transferencia com sucesso");
+		}else {
+			System.out.println("Faltou dinheiro pobre");
+		}
+		
+		System.out.println(outraConta.saldo);
+		System.out.println(conta.saldo);
+		
 
 	}
 
