d186c383a778da86a7de39e314d2818abfff603b
vijeremias <vjeremiasbraz@gmail.com>
2020-03-06 15:45:32 -0300

    ADIÇÃO

--- bytebank/src/bytebank/TestaMetodo.java
+++ bytebank/src/bytebank/TestaMetodo.java
@@ -5,7 +5,7 @@ public class TestaMetodo {
 	public static void main(String[] args) {
 		Conta conta  = new Conta();
 		conta.saldo = 2000;
-		conta.deposita(100);
+		conta.deposita(200);
 		System.out.println(conta.saldo);
 		// conta.saca(1);
 		boolean conseguiuRetirar = conta.saca(100);
