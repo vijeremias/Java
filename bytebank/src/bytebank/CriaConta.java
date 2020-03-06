6929101db769dffd28819f5d2d25e153af980b68
vijeremias <45375804+vijeremias@users.noreply.github.com>
2020-03-06 12:03:06 -0300

    Criação do projeto bytebank inicial

---
+++ bytebank/src/bytebank/CriaConta.java
@@ -0,0 +1,12 @@
+package bytebank;
+
+public class CriaConta {
+
+	public static void main(String[] args) {
+		Conta primeirConta = new Conta();
+		primeirConta.saldo = 200;
+		System.out.println(primeirConta.saldo);
+
+	}
+
+}
